99845c9b377457b18eea7593666a8f0c095351af
Tito Neves <nevtito@gmail.com>
2020-03-02 17:39:45 +0000

    Created testing grounds called playground

---
+++ app/src/main/java/com/sta847/stickcycle/ui/playground/PlaygroundFragment.java
@@ -0,0 +1,42 @@
+package com.sta847.stickcycle.ui.playground;
+
+import androidx.lifecycle.ViewModelProviders;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.sta847.stickcycle.R;
+
+public class PlaygroundFragment extends Fragment
+{
+
+    private PlaygroundViewModel mViewModel;
+
+    public static PlaygroundFragment newInstance()
+    {
+        return new PlaygroundFragment();
+    }
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState)
+    {
+        return inflater.inflate(R.layout.playground_fragment, container, false);
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState)
+    {
+        super.onActivityCreated(savedInstanceState);
+        mViewModel = ViewModelProviders.of(this).get(PlaygroundViewModel.class);
+        // TODO: Use the ViewModel
+    }
+
+}
---
+++ app/src/main/java/com/sta847/stickcycle/ui/playground/PlaygroundViewModel.java
@@ -0,0 +1,8 @@
+package com.sta847.stickcycle.ui.playground;
+
+import androidx.lifecycle.ViewModel;
+
+public class PlaygroundViewModel extends ViewModel
+{
+    // TODO: Implement the ViewModel
+}
