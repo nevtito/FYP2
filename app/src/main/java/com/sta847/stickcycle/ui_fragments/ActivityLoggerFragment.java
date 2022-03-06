3b1efdecebf81197d8179ca8fb89f029d28ec431
nevtito <nevtito@gmail.com>
2022-03-06 02:41:32 +0000

    Stopwatch changed to activity logger

--- app/src/main/java/com/sta847/stickcycle/ui_fragments/ActivityLoggerFragment.java
+++ app/src/main/java/com/sta847/stickcycle/ui_fragments/ActivityLoggerFragment.java
@@ -11,12 +11,12 @@ import androidx.fragment.app.Fragment;
 
 import com.sta847.stickcycle.R;
 
-public class StopwatchFragment extends Fragment
+public class ActivityLoggerFragment extends Fragment
 {
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState)
     {
-        View root = inflater.inflate(R.layout.fragment_stopwatch, container, false);
+        View root = inflater.inflate(R.layout.fragment_activity_logger, container, false);
         final TextView textView = root.findViewById(R.id.text_home);
         return root;
     }
